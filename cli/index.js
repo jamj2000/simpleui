1cc18f753f522319bbada967dd33d0a018dddfeb
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2026-07-27 17:58:42 +0200

    pubish

--- cli/index.js
+++ cli/index.js
@@ -5,11 +5,39 @@ import { init } from "./init.js";
 import { listComponents } from "./list.js";
 import { addComponents } from "./add.js";
 
+import { readFile } from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { Command } from "commander";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const packagePath = path.join(
+  __dirname,
+  "..",
+  "package.json"
+);
+
+const packageJson = JSON.parse(
+  await readFile(packagePath, "utf-8")
+);
+
+
 const program = new Command();
 
 program
   .name("simpleui")
-  .version("0.1.4");
+  .version(packageJson.version);
+
+program.parse();
+
+// const program = new Command();
+
+// program
+//   .name("simpleui")
+//   .version("0.1.4");
+
 
 program
   .command("init")
