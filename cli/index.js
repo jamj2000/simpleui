7054c737e09619fdfb1f4d593f039f49c4f5afda
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2026-07-30 23:26:35 +0200

    fix bug

--- cli/index.js
+++ cli/index.js
@@ -5,10 +5,10 @@ import { init } from "./init.js";
 import { listComponents } from "./list.js";
 import { addComponents } from "./add.js";
 
-import { readFile } from "node:fs/promises";
 import path from "node:path";
+import { readFile } from "node:fs/promises";
 import { fileURLToPath } from "node:url";
-import { Command } from "commander";
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
