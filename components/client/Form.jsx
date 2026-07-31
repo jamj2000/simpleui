716c0b954e08f1956989bf4f171d0590e8c6d81b
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2026-07-31 19:36:35 +0200

    InputGroup & InputSelect redefined

--- components/client/Form.jsx
+++ components/client/Form.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useActionState, useEffect, useRef, useState } from "react";
-// import { toast } from "sonner";
+import { toast } from "sonner";
 
 // Importamos todos los componentes de UI disponibles
 import { InputText } from "../server/InputText";
@@ -9,17 +9,20 @@ import { InputNumber } from "../server/InputNumber";
 import { InputGroup } from "../server/InputGroup";
 import { Submit } from "../server/Submit";
 import { Badge } from "../server/Badge";
-import { Alert } from "../server";
+import { Alert } from "../server/Alert";
+import { InputHidden } from "../server/InputHidden";
 import { InputSelect } from ".";
 
 
 
 // Creamos el mapa que relaciona el nombre (String) con el componente (React)
 const COMPONENT_MAP = {
+    InputHidden,
     InputText,
     InputNumber,
     InputGroup,
-    InputSelect
+    InputSelect,
+    Submit
 };
 
 
@@ -47,11 +50,20 @@ const fields = [
         label: "Habilidades",
         component: "InputGroup",
         radio: false,
-        values: [
-            ["leer", false],
-            ["cine", true],
-            ["música", true],
-            ["deporte", false]
+        options: [
+            ["Leer", "leer", false],
+            ["Cine", "cine", false],
+            ["Música", "música", true],
+            ["Deporte", "deporte", false]
+        ]
+    },
+    {
+        name: "tipo",
+        label: "Tipo",
+        component: "InputSelect",
+        options: [
+            ["Natural", "natural"],
+            ["Jurídico", "juridico"],
         ]
     },
 ];
@@ -70,31 +82,25 @@ export const Form = ({
     const [state, formAction, isPending] = useActionState(action, null);
     const [showMessage, setShowMessage] = useState(false);
     const formRef = useRef(null);
+    const handledStateRef = useRef(state);
 
+    console.log("data form", data)
 
     useEffect(() => {
         if (!state) return;
+        if (state === handledStateRef.current) return;
+
+        handledStateRef.current = state;
 
         if (state.message && state.type) {
-            // toast[state.type](state.message);
-
-            setShowMessage(true);
-
-            const timer = setTimeout(() => {
-                setShowMessage(false);
-                if (state.type == "success" || state.type == "info") {
-                    formRef.current?.closest("dialog")?.close();
-                }
-            }, 2000);
-
-            return () => clearTimeout(timer);
+            toast[state.type](state.message);
+            formRef.current?.closest("dialog")?.close();
         }
 
     }, [state]);
 
     return (
         <form ref={formRef} action={formAction} className={className}>
-            {data.id && <input type="hidden" name="id" defaultValue={data.id} />}
 
             {showMessage && <Alert type={state?.type}> {state?.message} </Alert>}
 
@@ -117,9 +123,7 @@ export const Form = ({
                                 {...field} // Pasa cualquier otro campo
                             />
 
-                            {errorCampo && (
-                                <Badge type="error">{errorCampo}</Badge>
-                            )}
+                            {errorCampo && <Badge type="error">{errorCampo}</Badge>}
                         </div>
                     );
                 })
