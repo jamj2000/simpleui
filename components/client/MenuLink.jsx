36c2f5af3fa7e1cfec7a348413ab100cbd1f9b6b
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2026-08-10 17:48:45 +0200

    ...

--- components/client/MenuLink.jsx
+++ components/client/MenuLink.jsx
@@ -9,7 +9,6 @@ export function MenuLink({ href, children }) {
 
     return (
         <Link
-            prefetch
             href={href}
             className={`peer-not-checked:hidden flex gap-2 items-center px-4 py-2 rounded-full active:bg-amber-700  hover:bg-slate-600 hover:text-white transition duration-300 ${pathname.endsWith(href) && 'bg-slate-700 text-white'}`}
         >
